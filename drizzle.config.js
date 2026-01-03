5a9609ffb7f8382fe2ed2fed03239a83dda0ff7b
Dipto-at-sap <dipto.das@sap.com>
2026-01-03 19:52:07 +0530

    Lint fixes

--- drizzle.config.js
+++ drizzle.config.js
@@ -7,11 +7,11 @@ config({ path: envFile });
 const isProduction = process.env.NODE_ENV === "production";
 
 export default {
-    schema: "./src/models/*.js",
-    out: "./drizzle",
-    dialect: "postgresql",
-    dbCredentials: {
-        url: process.env.DATABASE_URL,
-        ssl: isProduction ? "require" : false,
-    },
+  schema: "./src/models/*.js",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+    ssl: isProduction ? "require" : false,
+  },
 };
--- eslint.config.js
+++ eslint.config.js
@@ -3,76 +3,76 @@ import prettier from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
 
 export default [
-    js.configs.recommended,
-    prettierConfig,
-    {
-        files: ["**/*.js"],
-        languageOptions: {
-            ecmaVersion: "latest",
-            sourceType: "module",
-            globals: {
-                console: "readonly",
-                process: "readonly",
-                __dirname: "readonly",
-                __filename: "readonly",
-                Buffer: "readonly",
-                setImmediate: "readonly",
-                clearImmediate: "readonly",
-                setTimeout: "readonly",
-                clearTimeout: "readonly",
-                setInterval: "readonly",
-                clearInterval: "readonly",
-            },
-        },
-        plugins: {
-            prettier,
-        },
-        rules: {
-            "prettier/prettier": [
-                "error",
-                {
-                    useTabs: false,
-                    tabWidth: 2,
-                    printWidth: 100,
-                },
-            ],
-            "no-console": "off",
-            "no-unused-vars": [
-                "error",
-                {
-                    argsIgnorePattern: "^_",
-                    varsIgnorePattern: "^_",
-                },
-            ],
-            "no-undef": "error",
-            "prefer-const": "error",
-            "no-var": "error",
-            "object-shorthand": ["error", "always"],
-            "prefer-template": "error",
-            "prefer-arrow-callback": "error",
-            "no-duplicate-imports": "error",
-            eqeqeq: ["error", "always"],
-            "no-throw-literal": "error",
-            "prefer-promise-reject-errors": "error",
-        },
+  js.configs.recommended,
+  prettierConfig,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        Buffer: "readonly",
+        setImmediate: "readonly",
+        clearImmediate: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+      },
     },
-    {
-        files: ["**/*.test.js", "**/*.spec.js"],
-        languageOptions: {
-            globals: {
-                describe: "readonly",
-                it: "readonly",
-                test: "readonly",
-                expect: "readonly",
-                beforeEach: "readonly",
-                afterEach: "readonly",
-                beforeAll: "readonly",
-                afterAll: "readonly",
-                jest: "readonly",
-            },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          useTabs: false,
+          tabWidth: 2,
+          printWidth: 100,
         },
+      ],
+      "no-console": "off",
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "no-undef": "error",
+      "prefer-const": "error",
+      "no-var": "error",
+      "object-shorthand": ["error", "always"],
+      "prefer-template": "error",
+      "prefer-arrow-callback": "error",
+      "no-duplicate-imports": "error",
+      eqeqeq: ["error", "always"],
+      "no-throw-literal": "error",
+      "prefer-promise-reject-errors": "error",
     },
-    {
-        ignores: ["node_modules/**", "dist/**", "build/**", "*.config.js"],
+  },
+  {
+    files: ["**/*.test.js", "**/*.spec.js"],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        jest: "readonly",
+      },
     },
+  },
+  {
+    ignores: ["node_modules/**", "dist/**", "build/**", "*.config.js"],
+  },
 ];
